ddba04e9ad10ec6dee649dc3b4666e005e08a1ad
Denis <dzianis.pipko@gmail.com>
2025-06-23 19:30:11 +0300

    creating a store

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,8 @@ import { AuthLayout } from './Layout/Auth/AuthLayout.tsx'
 import { Login } from './pages/Login/Login.tsx'
 import { Register } from './pages/Register/Register.tsx'
 import { RequireAuth } from './helpers/RequireAuth.tsx'
+import { Provider } from 'react-redux'
+import { store } from './store/store.ts'
 
 const Menu = lazy(() => import('./pages/Menu/Menu.tsx'))
 
@@ -68,6 +70,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
